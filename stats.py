0bbfa55d2a4dc87f462b27e2a844d5e653c912aa
queenieroxorz <amybrown1130@gmail.com>
2017-01-14 11:01:16 -0600

    update

--- stats.py
+++ stats.py
@@ -35,33 +35,31 @@ df['Tobacco'] = df['Tobacco'].astype(float)
 
 mean_alc = df['Alcohol'].mean()
 med_alc = df['Alcohol'].median()
-mode_alc = stats.mode(df['Alcohol']) # mode returns the smallest number when there is not any numbers that occur mt once
+mode_alc = str(stats.mode(df['Alcohol'])) # mode returns the smallest number when there is not any numbers that occur mt once
 range_alc = max(df['Alcohol']) - min(df['Alcohol']) #obtains range
 std_alc = df['Alcohol'].std() #obtains standard deviation
 var_alc = df['Alcohol'].var()   # obtains variance
 
 mean_tob = df['Tobacco'].mean()
 med_tob = df['Tobacco'].median()
-mode_tob = stats.mode(df['Tobacco'])
+mode_tob = str(stats.mode(df['Tobacco']))
 range_tob = max(df['Tobacco']) - min(df['Tobacco']) #obtains range
 std_tob = df['Tobacco'].std() #obtains standard deviation
 var_tob = df['Tobacco'].var()
 
 dfname='Alcohol and Tobacco dataset'
 
-print('The mean weekly household spending on alcohol from the ' + dfname + ' is ' + str(mean_alc) + '.')
+print('The mean weekly household spending on alcohol from the ' + dfname + ' is ' + str(round(mean_alc, 2)) + '.')
 print('The median spending on alcohol from the ' + dfname + ' is ' + str(med_alc) + '.')
-print('The mode on alcohol from the ' + dfname + ' is ' + str(mode_alc) + '.') # this one needs fixing
+print('The mode on alcohol from the ' + dfname + ' is ' + mode_alc[24:28] + '.')
 print('The range on weekly household spending on alcohol is ' + str(range_alc) + '.')
-# ranged from ' + str(min(df['Alcohol'])) + 
-#' to ' +  str(max(df['Alcohol'])) + ', for a range of ' + str(range(_alc)))
-print('The variance of the weekly household spending on alcohol is ' + str(var_alc) + '.')
-print('The standard deviation of the weekly household spending on alcohol is ' + str(std_alc) + '.')
+print('The variance of the weekly household spending on alcohol is ' + str(round(var_alc, 2)) + '.')
+print('The standard deviation of the weekly household spending on alcohol is ' + str(round(std_alc, 2)) + '.')
 
 
-print('The mean weekly household spending on tobacco from the ' + dfname + ' is ' + str(mean_tob) + '.')
+print('The mean weekly household spending on tobacco from the ' + dfname + ' is ' + str(round(mean_tob, 2)) + '.')
 print('The median spending on tobacco from the ' + dfname + ' is ' + str(med_tob) + '.')
-print('The mode on tobacco from the ' + dfname + ' is ' + str(mode_tob) + '.')
+print('The mode on tobacco from the ' + dfname + ' is ' + mode_tob[24:28] + '.')
 print('The range on weekly household spending on tobacco is ' + str(range_tob) + '.')
-print('The variance of the tobacco spending is ' + str(var_tob) + '.')
-print('The standard deviation of tobacco spending is ' + str(std_tob) + '.')
+print('The variance of the tobacco spending is ' + str(round(var_tob, 2)) + '.')
+print('The standard deviation of tobacco spending is ' + str(round(std_tob, 2)) + '.')
